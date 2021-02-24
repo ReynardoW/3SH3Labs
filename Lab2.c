a3728822a8494dc01d87c569ebfe60b4e8526544
ReynardoW <widjajar@mcmaster.ca>
2021-02-24 16:42:24 -0500

    Lab2 Completed

--- Lab2.c
+++ Lab2.c
@@ -4,10 +4,12 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+
 int main(){
-  int fd[2], nbytes;
+  int fdChild[2], fdParent[2], nbytes;
   pid_t childpid;  
-  pipe(fd);
+  pipe(fdParent);
+  pipe(fdChild);
   childpid = fork();
   int sum = 0;
   int input;
@@ -15,45 +17,45 @@ int main(){
     perror("fork failed");
     exit(0);
   }
-  if (childpid == 0){
-    // Child process
-    int input;
-    printf("Input a 1-byte integer\n");
-    scanf("%d", &input);
-    if(input == -1){
-      // Send -1 to parent so it knows to send sum to child
-      write(fd[1], &input, sizeof(input));
-      open(fd[0]);
-      close(fd[1]);        
-      // Write parent (sum) into child process and print it
-      read(fd[0], &input, sizeof(input)); 
-      printf("sum = %d\n", input);
-      close(fd[0]);
-      exit(0);
-    }else {
-      //Write to parent
-      open(fd[1]);
-      close(fd[0]);
-      write(fd[1], &input, sizeof(input));
+  while(1){
+    if (childpid == 0){
+      // Child process
+      int input;
+      printf("Input a 1-byte integer\n");
+      scanf("%d", &input);
+      if(input == -1){
+        // Send -1 to parent so it knows to send sum to child
+        write(fdParent[1], &input, sizeof(input));
+        close(fdChild[1]);        
+        // Write parent (sum) into child process and print it
+        read(fdChild[0], &input, sizeof(input)); 
+        printf("sum = %d\n", input);
+        exit(0);
+      }else {
+        //Write to parent
+        close(fdParent[0]);   
+        close(fdChild[1]);
+        write(fdParent[1], &input, sizeof(input));
+      }
     }
-  }
-  if(childpid > 0){
-    // Parent process
-    // Read from child
-    open(fd[0]);
-    close(fd[1]);
-    int childInput;
-    read(fd[0], &childInput, sizeof(childInput));
-    if(childInput != -1){ // Read then write to child
-      open(fd[1]);
-      close(fd[0]);
-      write(fd[1], &sum, sizeof(sum));
-      close(fd[1]);
-      // Wait for child to exit then exit parent
-    }else{
-      sum += childInput;
+    if(childpid > 0){
+      // Parent process
+      // Read from child
+      int childInput;
+      close(fdParent[1]);
+      read(fdParent[0], &childInput, sizeof(childInput));
+      printf("Parent received: %d\n", childInput);
+      if(childInput == -1){ // Read then write to child
+        close(fdParent[0]);
+        write(fdChild[1], &sum, sizeof(sum));
+        // Wait for child to exit then exit parent
+        sleep(2);
+        exit(0);
+      }else{
+        sum += childInput;
+        printf("new sum = %d\n", sum);
+      }
     }
-  }
-    
-  return 0;
+  }  
+    return 0;
 }
